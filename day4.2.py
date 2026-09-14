20a5666db88173e2ff3bc3a3837a4cae7b415d06
T1.winds <153503409+KICmaofu@users.noreply.github.com>
2026-09-15 01:00:21 +0800

    1

--- day4.2.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-print("transformers导入成功！")
-# 测试文本分类小模型
-classifier = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
-res = classifier("I like deep learning")
-print(res)
